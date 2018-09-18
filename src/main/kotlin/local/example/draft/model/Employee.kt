c09dc94d89b865dff024e47d929920f82eb4f38f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-09-18 09:34:50 +0200

    Create Employee.kt

---
+++ src/main/kotlin/local/example/draft/model/Employee.kt
@@ -0,0 +1,44 @@
+/**
+ *
+ * Copyright 2018 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.draft.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "employee")
+data class Employee(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0,
+        var firstName: String?,
+        var lastName: String?,
+        var role: String?
+) {
+    lateinit var name: String
+
+    init {
+        if (!this.firstName.isNullOrBlank() && !this.lastName.isNullOrBlank()) {
+            this.name = this.firstName + " " + this.lastName
+        }
+        else if (!this.firstName.isNullOrBlank() && this.lastName.isNullOrBlank()) {
+            this.name = this.firstName!!
+        }
+        else {
+            this.name = this.lastName!!
+        }
+    }
+}
